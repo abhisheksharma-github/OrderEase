e31a8d4bde33cc0ee55ff9e0e6f3fad7de718590
Abhishek Sharma <abhisheksharma1808@gmail.com>
2025-03-24 21:22:34 +0530

    second commit

--- server/mailtrap/mailtrap.ts
+++ server/mailtrap/mailtrap.ts
@@ -11,3 +11,18 @@ export const sender = {
   email: "hello@demomailtrap.co",
   name: "OrderEase",
 };
+const recipients = [
+  {
+    email: "abhishekadarsh1808@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
